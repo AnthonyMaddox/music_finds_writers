57612e8869371cf5b180a3767751b7a73f6155f9
AnthonyMaddox <anthonyjmaddox@gmail.com>
2022-12-16 18:48:33 -0500

    initialized project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>The Music That Finds Us: Writer's Corner</div>
+    </div>
+  );
+}
+
+export default App;
